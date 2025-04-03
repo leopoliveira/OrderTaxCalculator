fe1b62335381276400af57aa5aa758746ec444cb
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2025-04-02 23:05:12 -0300

    feat: adds global error handling.

---
+++ OrderTaxCalculator.API/Errors/ProblemException.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderTaxCalculator.API.Errors;
+
+[Serializable]
+public class ProblemException : Exception
+{
+    public ProblemException(string erro, string mensagem) : base(mensagem)
+    {
+        Erro = erro;
+        Mensagem = mensagem;
+    }
+    
+    public string Erro { get; set; }
+
+    public string Mensagem { get; set; }
+}
